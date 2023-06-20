2f9fc061f17cd39d564e4895be17a5d302672b9e
yumekiti <yumekiti120440@gmail.com>
2023-06-21 05:22:38 +0900

    `change` allow_origins

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://marplify.yumekiti.net/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

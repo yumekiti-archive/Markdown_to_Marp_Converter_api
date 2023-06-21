3a0e70215fd9435f6cb63a9b45f17d8c943fcafb
Riku Miyata <amaguri0408@gmail.com>
2023-06-21 22:25:51 +0900

    `add` style api

---
+++ models/marp.py
@@ -0,0 +1,4 @@
+from pydantic import BaseModel
+
+class Marp(BaseModel):
+    theme: str
--- routers/markdown_router.py
+++ routers/markdown_router.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from controllers.markdown_controller import convert_markdown_to_marp
 from controllers.markdown_controller import convert_marp_to_markdown
+from controllers.marp_controller import get_theme
 from models.markdown import Markdown
+from models.marp import Marp
 
 router = APIRouter()
 
@@ -12,3 +14,7 @@ def markdown_to_marp(markdown: Markdown):
 @router.post("/marp-to-markdown")
 def marp_to_markdown(marp: Markdown):
     return convert_marp_to_markdown(marp.content)
+
+@router.post("/style")
+def change_theme(marp: Marp):
+    return get_theme(marp.theme)

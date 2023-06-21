eac8585815843331cbf746f267a416c36f53d466
kaneko1102 <kaneko-takuya@ed.tmu.ac.jp>
2023-06-21 21:09:19 +0900

    add export markdown to html

--- routers/markdown_router.py
+++ routers/markdown_router.py
@@ -2,6 +2,12 @@ from fastapi import APIRouter
 from controllers.markdown_controller import convert_markdown_to_marp
 from controllers.markdown_controller import convert_marp_to_markdown
 from models.markdown import Markdown
+from services.data_export import md_export
+from services.data_export import marp_export
+
+from pathlib import Path
+from datetime import datetime
+from fastapi.responses import FileResponse 
 
 router = APIRouter()
 
@@ -12,3 +18,35 @@ def markdown_to_marp(markdown: Markdown):
 @router.post("/marp-to-markdown")
 def marp_to_markdown(marp: Markdown):
     return convert_marp_to_markdown(marp.content)
+
+@router.post("/markdown-to-html")
+def markdown_to_html(markdown: Markdown):
+    md_export(markdown.content)
+    current = Path()
+    filename = 'md_export.html'
+    file_path =f'{current}/{filename}'
+    
+    now = datetime.now()
+    
+    response = FileResponse(
+                            path=file_path,
+                            filename=f"download_{now.strftime('%Y%m%d%H%M%S')}_{filename}"
+                            )
+ 
+    return response
+
+@router.post("/marp-to-html")
+def marp_to_html(marp: Markdown):
+    marp_export(marp.content)
+    current = Path()
+    filename = 'marp_export.html'
+    file_path =f'{current}/{filename}'
+    
+    now = datetime.now()
+    
+    response = FileResponse(
+                            path=file_path,
+                            filename=f"download_{now.strftime('%Y%m%d%H%M%S')}_{filename}"
+                            )
+ 
+    return response

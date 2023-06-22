14784f9862cae2c00386c16cb153542ab00bc724
Riku Miyata <amaguri0408@gmail.com>
2023-06-23 03:21:07 +0900

    code formatting

---
+++ controllers/export_controller.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+from datetime import datetime
+
+from fastapi.responses import FileResponse 
+
+from services.data_export import MarkdownExport, MarpExport
+
+def md_export(markdown_content: str):
+    MarkdownExport.md_export(markdown_content)
+    current = Path()
+    filename = 'md_export.html'
+    file_path =f'{current}/{filename}'
+    
+    now = datetime.now()
+    
+    response = FileResponse(
+                            path=file_path,
+                            filename=f"download_{now.strftime('%Y%m%d%H%M%S')}_{filename}"
+                            )
+ 
+    return response
+
+def marp_export(marp_content: str):
+    MarpExport.marp_export(marp_content)
+    current = Path()
+    filename = 'marp_export.html'
+    file_path =f'{current}/{filename}'
+    
+    now = datetime.now()
+    
+    response = FileResponse(
+                            path=file_path,
+                            filename=f"download_{now.strftime('%Y%m%d%H%M%S')}_{filename}"
+                            )
+ 
+    return response
